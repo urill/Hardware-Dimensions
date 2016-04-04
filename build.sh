794c1665767cff9da82342e9a851f45137a58329
madhephaestus <mad.hephaestus@gmail.com>
2016-04-03 21:21:42 -0400

    Cleaning up the directory to only being json

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-echo "Converting ODS to CSV..."
-libreoffice --headless --convert-to csv --outdir ./csv ./ods/*.ods
-echo "Starting CSV to JSON conversion script..."
-./csv_to_json.py
-echo "Finished!"
-
